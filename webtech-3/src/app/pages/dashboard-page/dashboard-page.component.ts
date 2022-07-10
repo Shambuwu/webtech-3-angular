5e65d678e937709adb3f93bb278a9c279c82f271
Shambuwu <Balfappius@protonmail.ch>
2022-07-10 12:19:40 +0200

    Laatste versie

---
+++ webtech-3/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {GamesService} from "../../services/games/games.service";
+
+@Component({
+  selector: 'app-dashboard-page',
+  templateUrl: './dashboard-page.component.html',
+  styleUrls: ['./dashboard-page.component.scss']
+})
+export class DashboardPageComponent implements OnInit {
+
+  constructor(private service: GamesService) { }
+
+  allGames: any = [];
+
+  ngOnInit(): void {
+    this.games();
+  }
+
+  games(): void {
+    this.service
+      .games()
+      .subscribe((response: any) => {
+        this.allGames = response.data;
+      });
+    console.log(this.allGames);
+  }
+
+}
