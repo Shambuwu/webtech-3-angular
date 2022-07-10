fd416b42637ffb01b89e1d33fb284f026b24c99c
chyrt <geertevertperton@gmail.com>
2022-07-10 14:50:56 +0200

    Geert branch first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webtech-3-angular
+
+Navigate into the directory  `webtech-3` to read the correct README.md
--- webtech-3/src/app/app.module.ts
+++ webtech-3/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    SubscribeComponent,
   ],
   imports: [
     BrowserModule,
